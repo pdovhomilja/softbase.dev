80cc92ff804a640a5156f1d6ebee3827c8c231c3
Pavel Dovhomilja <pavel@dovhomilja.cz>
2024-02-07 16:38:23 +0100

    fix: switch menu to server component

--- app/[locale]/(marketing)/_components/menu-client.tsx
+++ app/[locale]/(marketing)/_components/menu-client.tsx
@@ -1,23 +1,17 @@
-'use client'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
 
 const MenuClient = ({
   routes
 }: {
   routes: { name: string; path: string; active: boolean }[]
 }) => {
-  const path = usePathname()
-  const pathName = path.split('/')[2]
-
   return (
     <div className=' hidden gap-2 md:flex'>
       {routes
         .map(route => {
           return (
-            <Link key={route.path} href={route.path}>
+            <Link key={route.path} href={route.path} aria-label={route.name}>
               <Button variant={'outline'}>{route.name}</Button>
             </Link>
           )
