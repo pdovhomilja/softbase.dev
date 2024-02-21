ce0939111f9589145e12b4a55e7fedb3a1eb46c3
Pavel Dovhomilja <pavel@dovhomilja.cz>
2024-02-21 15:45:29 +0100

    fix:delete duplicated not-found

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import createMiddleware from 'next-intl/middleware'
-import { locales, localePrefix, pathnames } from './navigation'
 import { NextRequest, NextResponse } from 'next/server'
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
+
+import { locales, localePrefix, pathnames } from './navigation'
 
 export async function middleware(request: NextRequest) {
   const handleI18nRouting = createMiddleware({
@@ -60,9 +61,7 @@ export async function middleware(request: NextRequest) {
       }
     }
   )
-
   await supabase.auth.getUser()
-
   return response
 }
 
@@ -70,15 +69,12 @@ export const config = {
   matcher: [
     // Enable a redirect to a matching locale at the root
     '/',
-
     // Set a cookie to remember the previous locale for
     // all requests that have a locale prefix
     '/(cs|en)/:path*',
-
     // Enable redirects that add missing locales
     // (e.g. `/pathnames` -> `/en/pathnames`)
     //'/((?!_next|_vercel|.*\\..*).*)'
-
     //exclude /api route
     '/((?!_next|_vercel|.*\\..*|api).*)'
   ]
